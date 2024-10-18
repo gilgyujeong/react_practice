2e33b36d684b8f91a833da1a160fc64060fda021
gilgyujeong <jgg7645@naver.com>
2024-10-19 02:14:27 +0900

    24-10-19

--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -1,11 +1,17 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, ApolloLink } from '@apollo/client';
+import createUploadLink from 'apollo-upload-client/createUploadLink.mjs'
 
 interface IApolloSettingProps {
     children: JSX.Element;
 }
 
 export default function ApolloSetting(props: IApolloSettingProps):JSX.Element {
+    const uploadLink = createUploadLink({
+        uri: "http://backend-practice.codebootcamp.co.kr/graphql",
+    })
+    
     const client = new ApolloClient({
+        link: ApolloLink.from([uploadLink]),
         uri: "http://backend-practice.codebootcamp.co.kr/graphql",
         cache: new InMemoryCache()  // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기
     })
