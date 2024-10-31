564c718a71f5e981de81802aeafc88f45c18ed84
gilgyujeong <jgg7645@naver.com>
2024-11-01 01:46:25 +0900

    24.10.31

--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -1,5 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, ApolloLink } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client'
+import { useRecoilState } from 'recoil';
+import { accessTokenState } from '../../../commons/stores';
 
 const GLOBAL_STATE = new InMemoryCache();
 
@@ -8,8 +10,13 @@ interface IApolloSettingProps {
 }
 
 export default function ApolloSetting(props: IApolloSettingProps):JSX.Element {
+    const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+    
     const uploadLink = createUploadLink({
         uri: "http://backend-practice.codebootcamp.co.kr/graphql",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        }
     })
     
     const client = new ApolloClient({
